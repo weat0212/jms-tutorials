1c1e1721b602cea9aac5cc9037dbf386cc400fc6
andy <andy.wang@webcomm.com.tw>
2023-01-07 17:42:59 +0800

    use replyTo

--- src/main/java/com/andywang/jms/message/RequestReplyDemo.java
+++ src/main/java/com/andywang/jms/message/RequestReplyDemo.java
@@ -4,14 +4,16 @@ import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
 
 import javax.jms.JMSConsumer;
 import javax.jms.JMSContext;
+import javax.jms.JMSException;
 import javax.jms.JMSProducer;
 import javax.jms.Queue;
+import javax.jms.TextMessage;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
 public class RequestReplyDemo {
 
-    public static void main(String[] args) throws NamingException {
+    public static void main(String[] args) throws NamingException, JMSException {
 
         InitialContext initialContext = new InitialContext();
         Queue requestQueue = (Queue) initialContext.lookup("queue/requestQueue");
@@ -20,16 +22,22 @@ public class RequestReplyDemo {
         try(ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory();
             JMSContext jmsContext = cf.createContext()) {
 
+            // A: How do you do?
             JMSProducer producer = jmsContext.createProducer();
-            producer.send(requestQueue, "How do you do?");
+            TextMessage message = jmsContext.createTextMessage("How do you do?");
+            message.setJMSReplyTo(replyQueue);
+            producer.send(requestQueue, message);
 
+            // B: (Received Message)
             JMSConsumer consumer = jmsContext.createConsumer(requestQueue);
-            String messageReceived = consumer.receiveBody(String.class);
-            System.out.println("Received Request Message: " + messageReceived);
+            TextMessage messageReceived = (TextMessage) consumer.receive();
+            System.out.println("Received Request Message: " + messageReceived.getText());
 
+            // B: I'm fine, thank u.
             JMSProducer replyProducer = jmsContext.createProducer();
-            replyProducer.send(replyQueue, "I'm fine, thank u.");
+            replyProducer.send(messageReceived.getJMSReplyTo(), "I'm fine, thank u.");
 
+            // A: (Received Message)
             JMSConsumer replyConsumer = jmsContext.createConsumer(replyQueue);
             String replyMessageReceived = replyConsumer.receiveBody(String.class);
             System.out.println("Received Reply Message: " + replyMessageReceived);
