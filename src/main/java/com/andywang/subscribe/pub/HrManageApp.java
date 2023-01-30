fbd4d302ee637a348fa9bab845ad3378031e9fb1
andy <andy.wang@webcomm.com.tw>
2023-01-30 17:26:06 +0800

    shared subscription

--- src/main/java/com/andywang/subscribe/pub/HrManageApp.java
+++ src/main/java/com/andywang/subscribe/pub/HrManageApp.java
@@ -7,6 +7,7 @@ import javax.jms.JMSContext;
 import javax.jms.Topic;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+import java.util.stream.IntStream;
 
 public class HrManageApp {
 
@@ -18,11 +19,13 @@ public class HrManageApp {
         try (ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory();
              JMSContext jmsContext =  cf.createContext()) {
 
-            Employee employee =
-                    Employee.builder().id(1).firstName("Andy").lastName("Wang")
-                            .designation("Software Engineer").email("weat0212@gmail.com").phone("0912345678").build();
+            IntStream.rangeClosed(1, 10).forEach(x -> {
+                Employee employee =
+                        Employee.builder().id(x).firstName("Andy").lastName("Wang")
+                                .designation("Software Engineer").email("weat0212@gmail.com").phone("0912345678").build();
+                jmsContext.createProducer().send(topic, employee);
+            });
 
-            jmsContext.createProducer().send(topic, employee);
             System.out.println("Message Sent");
         }
     }
--- src/main/java/com/andywang/subscribe/sub/WellnessApp.java
+++ src/main/java/com/andywang/subscribe/sub/WellnessApp.java
@@ -13,6 +13,8 @@ import javax.naming.NamingException;
 
 public class WellnessApp {
 
+    public static final String SUBSCRIPTION_NAME = WellnessApp.class.getSimpleName();
+
     public static void main(String[] args) throws NamingException, JMSException {
 
         InitialContext initialContext = new InitialContext();
@@ -21,10 +23,41 @@ public class WellnessApp {
         try (ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory();
              JMSContext jmsContext =  cf.createContext()) {
 
-            JMSConsumer consumer = jmsContext.createConsumer(topic);
-            Message receivedMessage = consumer.receive();
-            Employee employee = receivedMessage.getBody(Employee.class);
-            System.out.println(employee);
+            JMSConsumer consumer1 = jmsContext.createSharedConsumer(topic, SUBSCRIPTION_NAME);
+            JMSConsumer consumer2 = jmsContext.createSharedConsumer(topic, SUBSCRIPTION_NAME);
+
+            /*
+             * Shared Consumer
+             * get message take turn
+             */
+
+            for (int i = 0; i < 5; i++) {
+                Message receivedMessage1 = consumer1.receive();
+                Employee employee1 = receivedMessage1.getBody(Employee.class);
+                System.out.println("Consumer1: " + employee1);
+
+                Message receivedMessage2 = consumer2.receive();
+                Employee employee2 = receivedMessage2.getBody(Employee.class);
+                System.out.println("Consumer2: " + employee2);
+            }
+
+
+            /*
+             * Normal Consumer
+             * consumer both got same message
+             */
+//            JMSConsumer consumer1 = jmsContext.createConsumer(topic);
+//            JMSConsumer consumer2 = jmsContext.createConsumer(topic);
+//
+//            for (int i = 0; i < 10; i++) {
+//                Message receivedMessage1 = consumer1.receive();
+//                Employee employee1 = receivedMessage1.getBody(Employee.class);
+//                System.out.println("Consumer1: " + employee1);
+//
+//                Message receivedMessage2 = consumer2.receive();
+//                Employee employee2 = receivedMessage2.getBody(Employee.class);
+//                System.out.println("Consumer2: " + employee2);
+//            }
         }
     }
 }
