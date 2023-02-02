2301471ef38c201f6b9542795898e9762d3b7705
andy <andy.wang@webcomm.com.tw>
2023-02-02 15:37:04 +0800

    client and duplicate ok acknowledge

--- src/main/java/com/andywang/jms/guaranted/MessageConsumer.java
+++ src/main/java/com/andywang/jms/guaranted/MessageConsumer.java
@@ -1,6 +1,7 @@
 package com.andywang.jms.guaranted;
 
 import com.andywang.vo.CustomMessage;
+import lombok.SneakyThrows;
 import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
 
 import javax.jms.JMSConsumer;
@@ -15,12 +16,20 @@ public class MessageConsumer {
 
     public static void main(String[] args) throws NamingException, JMSException {
 
+        // default: AUTO_ACKNOWLEDGE
+
+        clientAcknowledge();
+//        dupOkAcknowledge();
+    }
+
+    @SneakyThrows
+    static void clientAcknowledge() {
+
         InitialContext initialContext = new InitialContext();
         Queue queue = (Queue) initialContext.lookup("queue/requestQueue");
 
         try (ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory();
              JMSContext jmsContext = cf.createContext(JMSContext.CLIENT_ACKNOWLEDGE)) {
-            // default: JMSContext.AUTO_ACKNOWLEDGE
 
             JMSConsumer consumer = jmsContext.createConsumer(queue);
 
@@ -31,6 +40,23 @@ public class MessageConsumer {
             // Try to commit this line, and run this twice.
             // Result: consumer will keep received same message until ack is sent back to JMS Provider
             receivedMessage.acknowledge();
+        }
+    }
+
+    @SneakyThrows
+    static void dupOkAcknowledge() {
+
+        InitialContext initialContext = new InitialContext();
+        Queue queue = (Queue) initialContext.lookup("queue/requestQueue");
+
+        try (ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory();
+             JMSContext jmsContext = cf.createContext(JMSContext.DUPS_OK_ACKNOWLEDGE)) {
+
+            JMSConsumer consumer = jmsContext.createConsumer(queue);
+
+            Message receivedMessage = consumer.receive();
+            CustomMessage customMessage = receivedMessage.getBody(CustomMessage.class);
+            System.out.println(customMessage.toString());
         }
     }
 }
